c4b1929e758e9ef5af26f44270263e3779177db0
jmichaels1 <michael.salr1590@gmail.com>
2018-05-01 12:50:45 +0200

    add sessions calendar interface

--- src/MainRank.java
+++ src/MainRank.java
@@ -17,11 +17,19 @@ public class MainRank extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        root = FXMLLoader.load(getClass().getResource("view/intCourseRank.fxml"));
+//        root = FXMLLoader.load(getClass().getResource("view/intCourseRank.fxml"));
+//        stage = primaryStage;
+//        stage.setTitle("Rango Stage");
+//        stage.setScene(new Scene(root, 495, 307));
+//        stage.show();
+
+
+        root = FXMLLoader.load(getClass().getResource("view/intCalendarSessions.fxml"));
         stage = primaryStage;
-        stage.setTitle("Rango Stage");
-        stage.setScene(new Scene(root, 495, 307));
+        stage.setTitle("Sessions Calendar");
+        stage.setScene(new Scene(root, 950 , 650));
         stage.show();
+
     }
 
     public static Stage getStage() {
