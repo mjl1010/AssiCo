c06cb9aadfac5cefe54219a87d0c6ba671a9f2a9
jmichaels1 <michael.salr1590@gmail.com>
2018-05-08 13:15:59 +0200

    remodeling sessions calendar 2

---
+++ src/model/GridSesion.java
@@ -0,0 +1,15 @@
+package model;
+
+import javafx.scene.layout.GridPane;
+
+import java.awt.*;
+
+/**
+ * Created by Michael
+ */
+public class GridSesion {
+
+    GridPane miniGrid;
+    Label lblDateID, lblAux1, lblAux2, lblAsign, lblContenido;
+
+}
