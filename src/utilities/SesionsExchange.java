9a60d7644b9e4d6be8111458cf37175c91da5ced
jmichaels1 <michael.salr1590@gmail.com>
2018-05-17 13:30:45 +0200

    commit 17/05

--- src/utilities/SesionsExchange.java
+++ src/utilities/SesionsExchange.java
@@ -2,11 +2,12 @@ package utilities;
 
 import entity.DiaPlanificado;
 import entity.Sesion;
-import utilities.TabCalendarMaster;
 
 import static utilities.VariablesAndMethodsUtils.getDiaPlanificado;
 
-
+/**
+ * Created by Michael
+ */
 public class SesionsExchange {
 
     private static TabCalendarMaster tcm;
@@ -14,22 +15,26 @@ public class SesionsExchange {
     private static DiaPlanificado dp_exch2;
     private static Sesion ses_aux;
 
-
+    /**
+     * change method
+     * @param tcm
+     * @param date1
+     * @param date2
+     */
     public static void change(TabCalendarMaster tcm, String date1, String date2) {
-//        System.out.println("tcm : " + tcm);
-//        System.out.println("date1 : " + date1);
-//        System.out.println("date2 : " + date2);
         dp_exch1 = getDiaPlanificado(date1, tcm);
-//        System.out.println("dp_exch1 : " + dp_exch1);
         dp_exch2 = getDiaPlanificado(date2, tcm);
-//        System.out.println("dp_exch2 : " + dp_exch2);
         ses_aux = dp_exch1.getSesion();
         procederChange(dp_exch1, dp_exch2);
         dp_exch2.setSesion(ses_aux);
         tcm.updateCalendar();
     }
 
-
+    /**
+     * procede Change
+     * @param dp1
+     * @param dp2
+     */
     private static void procederChange(DiaPlanificado dp1, DiaPlanificado dp2) {
         dp1.setSesion(dp2.getSesion());
     }
